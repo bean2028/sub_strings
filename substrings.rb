8cbaeebd5ffdd633056145bd886c7c692edab760
Eric Helstad <erichelstad@gmail.com>
2023-02-16 15:39:20 -0600

    Create hash and add key/value if dictionary word included

--- substrings.rb
+++ substrings.rb
@@ -3,11 +3,14 @@ require 'pry-byebug'
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(string, word_array)
+  my_hash = Hash.new
   string_array = string.split(" ")
   word_array.each do |word|
     string_array.each do |string_word|
-      bool = string_word.include?(word)
-      binding.pry
+      if string_word.include?(word) == true
+        my_hash[word] = 1
+        binding.pry
+      end
     end
   end
   
