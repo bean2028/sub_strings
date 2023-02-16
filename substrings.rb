bb661ce80c4c076fcff24b23b14b19a3cdfe4604
Eric Helstad <erichelstad@gmail.com>
2023-02-16 15:31:58 -0600

    Confirm enumeration through arrays is working

--- substrings.rb
+++ substrings.rb
@@ -4,7 +4,13 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 def substrings(string, word_array)
   string_array = string.split(" ")
-  binding.pry
+  word_array.each do |word|
+    string_array.each do |string_word|
+      bool = string_word.include?(word)
+      binding.pry
+    end
+  end
+  
 end
 
 substrings("Howdy partner, sit down! How's it going?", dictionary)
