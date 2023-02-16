08fb99917f6c90e4fc6fce0b93bebe51bf2f509b
Eric Helstad <erichelstad@gmail.com>
2023-02-16 15:51:42 -0600

    Add logic to increment values of hash keys

--- substrings.rb
+++ substrings.rb
@@ -3,17 +3,16 @@ require 'pry-byebug'
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(string, word_array)
-  my_hash = Hash.new
+  my_hash = Hash.new(0)
   string_array = string.split(" ")
   word_array.each do |word|
     string_array.each do |string_word|
       if string_word.include?(word) == true
-        my_hash[word] = 1
-        binding.pry
+        my_hash[word] += 1
       end
     end
   end
-  
+  puts my_hash
 end
 
 substrings("Howdy partner, sit down! How's it going?", dictionary)
