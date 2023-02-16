aace5ad1a169cd93790f034d0e64404c29b61ee7
Eric Helstad <erichelstad@gmail.com>
2023-02-16 15:25:32 -0600

    Convert string into an array of words

--- substrings.rb
+++ substrings.rb
@@ -1,7 +1,10 @@
 require 'pry-byebug'
 
-dictionary = []
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(string, word_array)
-
+  string_array = string.split(" ")
+  binding.pry
 end
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
